25415f278ab68f387e42834f3ee93fcaf5d3ae4b
Dylan Hicks <dylanh333@outlook.com>
2017-02-03 22:27:38 +0800

    New script that does both CPU frequency and temperature in one go!

---
+++ monitor-freqAndTemp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while [ 1 ]; do
+	clear
+
+	echo "-- CPU Frequencies"
+	ls /sys/devices/system/cpu/ | egrep "cpu[0-9]+" | while read cpu; do
+		printf '%s: %s kHz\n' "$cpu" "$(cat /sys/devices/system/cpu/$cpu/cpufreq/cpuinfo_cur_freq)"
+	done
+	echo ""
+
+	echo "-- Temperatures"
+	sensors coretemp-isa-0000
+
+	sleep 5
+done
