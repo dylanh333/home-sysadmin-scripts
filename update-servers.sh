12951bc9c35c211e6d4ad1204eab87018389f4a3
Dylan Hicks <dylanh333@outlook.com>
2017-05-25 14:03:51 +0800

    Re-jigged update-servers.sh to pretty print the success/fail status of each server (in colour), and made it treat server offline and other exit codes seperately. Failed updates now also get sent to stderr instead of stdout.

--- update-servers.sh
+++ update-servers.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
 
-exec 3< ~/.serverlist.txt
+# Serverlist file
+serverList=~/.serverlist.txt
 
-export TERM=xterm
+# Update commands
+updateCommands="sudo apt-get -y update; sudo apt-get -y upgrade; sudo apt-get -y autoremove"
+
+# Terminal text styling templates
+FORMAT_RESET="\x1b[0m"
+FORMAT_HEADING="\x1b[1;4m"
+FORMAT_SUCCESS="\x1b[42m"
+FORMAT_FAIL="\x1b[41m"
+function printHeading(){
+	printf "$FORMAT_HEADING"
+	printf "%s\n" "--- $1 ---"
+	printf "$FORMAT_RESET"
+}
+function printSuccess(){
+	printf "$FORMAT_SUCCESS"
+	printf "SUCCESS: "
+	printf "%s\n" "$1"
+	printf "$FORMAT_RESET"
+}
+function printFail(){
+	printf "$FORMAT_FAIL"
+	printf "FAIL: " >&2
+	printf "%s\n" "$1" >&2
+	printf "$FORMAT_RESET"
+}
+
+# Needed to prevent screen breaking TERM variable
+export TERM="xterm-color"
+
+# For each line of the server list
+exec 3< "$serverList"
 while read -u 3 server; do
-	echo "--- Updating $server"
+	printHeading "Updating $server"
 	
-	if ! (
-		ssh "$server" "sudo apt-get -y update; sudo apt-get -y upgrade; sudo apt-get -y autoremove" 2> /dev/null
-	) then
-		echo "$server is offline."
-	else
-		echo "$server is now up to date."
-	fi
+	ssh "$server" "$updateCommands" 2> /dev/null
+	exitCode="$?"
+	
+	case "$exitCode" in
+		255)
+			printFail "$server is offline."
+			;;
+		0)
+			printSuccess "$server is now up to date."
+			;;
+		*)
+			printFail "$server exited with status code $exitCode."
+			;;
+	esac
 
 	echo ""
 done
