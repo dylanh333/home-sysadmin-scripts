4cb1e5bc9df1ca9b5a0320df5c65093082b170b6
Dylan Hicks <dylanh333@outlook.com>
2018-10-14 16:21:06 +0800

    Changed the path used to poll the CPU frequency from the old "/sys/devices/system/cpu/$cpu/cpufreq/cpuinfo_cur_freq" to "./scaling_cur_freq", as this seems to have changed in Linux 4.15, at least in my ProxMox environment.

--- monitor-freq.sh
+++ monitor-freq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while [ 1 ]; do
 	clear
-	cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
+	cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq
 	sleep 5
 done
--- monitor-freqAndTemp.sh
+++ monitor-freqAndTemp.sh
@@ -5,7 +5,7 @@ while [ 1 ]; do
 
 	echo "-- CPU Frequencies"
 	ls /sys/devices/system/cpu/ | egrep "cpu[0-9]+" | while read cpu; do
-		printf '%s: %s kHz\n' "$cpu" "$(cat /sys/devices/system/cpu/$cpu/cpufreq/cpuinfo_cur_freq)"
+		printf '%s: %s kHz\n' "$cpu" "$(cat /sys/devices/system/cpu/$cpu/cpufreq/scaling_cur_freq)"
 	done
 	echo ""
 
